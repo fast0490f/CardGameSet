3bb55f65db5780bb9b01e78431c982dea521e02a
Артём Иванов <artem@informatica.ru>
2016-04-14 21:49:01 +0300

    update

--- src/html/js/card.js
+++ src/html/js/card.js
@@ -1,5 +1,8 @@
 
-function setCard(_x, _y) {
+function setCard(_x, _y, _quantity) {
+
+  let x = 0;
+  let y = 0;
 
   const t = new PIXI.Graphics();
   t.interactive = true;
@@ -10,34 +13,73 @@ function setCard(_x, _y) {
   .endFill();
 
 
-  let x = 1 + 3 + 0 + 10;
-  let y = 15;
-  t.beginFill(0xFFFFFF).lineStyle(2, 0x33CC66)
-    .moveTo(x + 35, y + 0)
-    .lineTo(x + 0, y + 55)
-    .lineTo(x + 35, y + 110)
-    .lineTo(x + 70, y + 55)
-  .endFill();
+  switch (_quantity) {
+    case 1:
+
+      x = 130 - 35;
+      y = 15;
+      t.beginFill(0x33CC66, 0.35).lineStyle(2, 0x33CC66)
+        .moveTo(x + 35, y + 0)
+        .lineTo(x + 0, y + 55)
+        .lineTo(x + 35, y + 110)
+        .lineTo(x + 70, y + 55)
+      .endFill();
+
+      break;
+    case 2:
+      x = 55;
+      y = 15;
+      t.beginFill(0xFFFFFF).lineStyle(2, 0x33CC66)
+        .moveTo(x + 35, y + 0)
+        .lineTo(x + 0, y + 55)
+        .lineTo(x + 35, y + 110)
+        .lineTo(x + 70, y + 55)
+      .endFill();
+
+      x = 55 + 12 + 70;
+      y = 15;
+      t.beginFill(0x33CC66, 0.35).lineStyle(2, 0x33CC66)
+        .moveTo(x + 35, y + 0)
+        .lineTo(x + 0, y + 55)
+        .lineTo(x + 35, y + 110)
+        .lineTo(x + 70, y + 55)
+      .endFill();
+
+      break;
+    case 3:
+
+      x = 1 + 3 + 0 + 10;
+      y = 15;
+      t.beginFill(0xFFFFFF).lineStyle(2, 0x33CC66)
+        .moveTo(x + 35, y + 0)
+        .lineTo(x + 0, y + 55)
+        .lineTo(x + 35, y + 110)
+        .lineTo(x + 70, y + 55)
+      .endFill();
+
+      x = 6 + 70 + 20;
+      y = 15;
+      t.beginFill(0x33CC66, 0.35).lineStyle(2, 0x33CC66)
+        .moveTo(x + 35, y + 0)
+        .lineTo(x + 0, y + 55)
+        .lineTo(x + 35, y + 110)
+        .lineTo(x + 70, y + 55)
+      .endFill();
 
 
-  x = 6 + 70 + 20;
-  y = 15;
-  t.beginFill(0x33CC66, 0.35).lineStyle(2, 0x33CC66)
-    .moveTo(x + 35, y + 0)
-    .lineTo(x + 0, y + 55)
-    .lineTo(x + 35, y + 110)
-    .lineTo(x + 70, y + 55)
-  .endFill();
+      x = 1 + 6 + 140 + 30;
+      y = 15;
+      t.beginFill(0x33CC66).lineStyle(2, 0x33CC66)
+        .moveTo(x + 35, y + 0)
+        .lineTo(x + 0, y + 55)
+        .lineTo(x + 35, y + 110)
+        .lineTo(x + 70, y + 55)
+      .endFill();
 
+      break;
+    default:
+  }
 
-  x = 1 + 6 + 140 + 30;
-  y = 15;
-  t.beginFill(0x33CC66).lineStyle(2, 0x33CC66)
-    .moveTo(x + 35, y + 0)
-    .lineTo(x + 0, y + 55)
-    .lineTo(x + 35, y + 110)
-    .lineTo(x + 70, y + 55)
-  .endFill();
 
   t.position.x = _x;
   t.position.y = _y;
--- src/html/js/init.js
+++ src/html/js/init.js
@@ -1,6 +1,6 @@
 const renderer = PIXI.autoDetectRenderer(
-  window.innerWidth - 250,
-  window.innerHeight - 20,
+  window.innerWidth - 50,
+  window.innerHeight - 10,
   { antialias: true, backgroundColor: 0x1099bb }
 );
 
@@ -9,8 +9,25 @@ const stage = new PIXI.Container();
 function init() {
   document.body.appendChild(renderer.view);
 
+  stage.addChild(setCard(20 * 1  + (1-1) * 260, 20, 1));
+  stage.addChild(setCard(20 * 2  + (2-1) * 260, 20, 1));
+  stage.addChild(setCard(20 * 3  + (3-1) * 260, 20, 1));
+  stage.addChild(setCard(20 * 4  + (4-1) * 260, 20, 1));
 
-  stage.addChild(setCard(100, 100));
+  stage.addChild(setCard(20 * 1  + (1-1) * 260, 2 * 20 + 140, 1));
+  stage.addChild(setCard(20 * 2  + (2-1) * 260, 2 * 20 + 140, 1));
+  stage.addChild(setCard(20 * 3  + (3-1) * 260, 2 * 20 + 140, 1));
+  stage.addChild(setCard(20 * 4  + (4-1) * 260, 2 * 20 + 140, 1));
+
+  stage.addChild(setCard(20 * 1  + (1-1) * 260, 3 * 20 + 140 *2, 1));
+  stage.addChild(setCard(20 * 2  + (2-1) * 260, 3 * 20 + 140 *2, 1));
+  stage.addChild(setCard(20 * 3  + (3-1) * 260, 3 * 20 + 140 *2, 1));
+  stage.addChild(setCard(20 * 4  + (4-1) * 260, 3 * 20 + 140 *2, 1));
+
+  stage.addChild(setCard(20 * 1  + (1-1) * 260, 4 * 20 + 140 *3, 1));
+  stage.addChild(setCard(20 * 2  + (2-1) * 260, 4 * 20 + 140 *3, 1));
+  stage.addChild(setCard(20 * 3  + (3-1) * 260, 4 * 20 + 140 *3, 1));
+  stage.addChild(setCard(20 * 4  + (4-1) * 260, 4 * 20 + 140 *3, 1));
 
 
   function animate() {
