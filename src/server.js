6fbe46b8f84c7d8defece0afed1057df25cd0e74
Артём Иванов <artem@informatica.ru>
2016-04-19 13:57:27 +0300

    update

--- src/server.js
+++ src/server.js
@@ -12,8 +12,31 @@ server.sendAll = data => {
   server.clients.forEach(client => client.send(JSON.stringify(data)));
 };
 
+function clientClose(client) {
+  const _ban = [];
+  const _users = [];
+  const _count = [];
+
+  ban[client._socket.remoteAddress + client._socket.remotePort] = false;
+  users[client._socket.remoteAddress + client._socket.remotePort] = false;
+  count[client._socket.remoteAddress + client._socket.remotePort] = false;
+
+  Object.keys(users).forEach((o, index) => {
+    if (users[index] !== false) {
+      _ban[index] = ban[index];
+      _users[index] = users[index];
+      _count[index] = count[index];
+    }
+  });
+  ban = _ban;
+  users = _users;
+  count = _count;
+  server.sendAll({ action: 'user', users, ban, count });
+}
+
 
 function clientConnection(client) {
+  count[client._socket.remoteAddress + client._socket.remotePort] = 0;
 
   if (Object.keys(server.clients).length === 1) {
     ban = {};
@@ -38,7 +61,7 @@ function clientSendMessage(client, mes) {
       const check = checkGame(mes.ob);
       if (check.action === 'win') {
         ban = {};
-        count[client._socket.remoteAddress + client._socket.remotePort] = count[client._socket.remoteAddress + client._socket.remotePort] || 0 + 3;
+        count[client._socket.remoteAddress + client._socket.remotePort] = count[client._socket.remoteAddress + client._socket.remotePort]  + 3;
         console.log(count[client._socket.remoteAddress + client._socket.remotePort]);
         server.sendAll({ action: 'game', game: check.game });
         server.sendAll({ action: 'user', users, ban, count });
@@ -61,6 +84,7 @@ function clientSendMessage(client, mes) {
 server.on('connection', client => {
   clientConnection(client);
   client.on('message', message => clientSendMessage(client, JSON.parse(message)));
+  client.on('close', message => clientClose(client));
 });
 
 if (server) {
