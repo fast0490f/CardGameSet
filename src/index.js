b2fc4408714b739e0a9379a21da9e8e83cf5b38f
fast0490f <fast0490f@mail.ru>
2016-05-06 14:30:48 +0300

    add index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+require('./server/run.js');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.set('json spaces', 2);
+
+app.use(express.static(`${__dirname}/html`));
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port);
+/* eslint no-console: 0 */
+console.warn('Http listening on port', port);
